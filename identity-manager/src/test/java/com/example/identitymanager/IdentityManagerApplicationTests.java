2514e46a92c522118aabfcf23ac1ad4a69154dba
Zofia Pietrucha <zosia.p.07@gmail.com>
2026-01-07 20:52:04 +0100

    Add model and application tests for full coverage

--- identity-manager/src/test/java/com/example/identitymanager/IdentityManagerApplicationTests.java
+++ identity-manager/src/test/java/com/example/identitymanager/IdentityManagerApplicationTests.java
@@ -8,6 +8,13 @@ class IdentityManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Verifies that Spring context loads correctly
 	}
 
+	@Test
+	void mainMethodStartsApplication() {
+		// This test covers the main() method
+		// We use empty args as we don't need to actually start the full server
+		IdentityManagerApplication.main(new String[]{});
+	}
 }
